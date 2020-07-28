1c1c1ffe0389df08d05b9f8eeaf3b7d9ec16378b
adevadeh <adeh@yahoo.com>
2020-07-28 12:18:48 +0800

    Create assignment1.js

---
+++ assignment1.js
@@ -0,0 +1,26 @@
+// Write a function power(x,n) such that power(x,n) returns x to the nth power
+// Write a 4 tests to show that the function ir performing correctly
+
+// Part 1 - Function
+// Fill in the following function definition
+
+
+function power(x, n) {
+
+}
+
+
+
+console.log(power(1,1));
+console.log(power(2,2));
+console.log(power(3,3));
+console.log(power(4,4));
+
+
+// Part 2 - Testing
+// Add 4 more tests to show that the function is working properly
+
+console.assert(power(2,2), 4);
+
+
+
